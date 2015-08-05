fc23784c5156bb20cb863be43955d41a09538a21
Derrick Ma <DMA>
2015-08-05 19:13:24 +1200

    Create the project
    Create the project

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,455 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    [Range(0, 5)]
+    public int borderSize;
+
+    [Range(0, 100)]
+    public int randomFillPercent;
+
+    [Range(0, 5)]
+    public int smoothTimes;
+
+    public string seed;
+    public bool useRandomSeed;
+
+    [Range(0, 50)]
+    public int threshold = 30;
+
+    int[,] map;
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GenerateMap();
+        }
+    }
+
+    void GenerateMap()
+    {
+        map = new int[width, height];
+        randomFillMap();
+
+        for (int i = 0; i < smoothTimes; i++)
+        {
+            SmoothMap();
+        }
+
+        ProcessMap();
+
+        MeshGenerator meshGen = GetComponent<MeshGenerator>();
+        meshGen.GenerateMesh(map, 1f);
+    }
+
+    //void OnDrawGizmos()
+    //{
+    //    if (map != null)
+    //    {
+    //        for (int x = 0; x < width; x++)
+    //        {
+    //            for (int y = 0; y < height; y++)
+    //            {
+    //                Gizmos.color = (map[x, y] == 1) ? Color.black : Color.white;
+    //                Vector3 pos = new Vector3(-width / 2 + x + .5f, -5, -height / 2 + y + .5f);
+    //                Gizmos.DrawCube(pos, Vector3.one);
+    //            }
+    //        }
+    //    }
+    //}
+
+
+    List<Coord> GetRegionTiles(int startX, int startY, int[,] mapFlags)
+    {
+        List<Coord> tileList = new List<Coord>();
+        int tileType = map[startX, startY];
+
+        Queue<Coord> queue = new Queue<Coord>();
+
+        mapFlags[startX, startY] = 1;
+        queue.Enqueue(new Coord(startX, startY));
+
+        while (queue.Count > 0)
+        {
+            var tile = queue.Dequeue();
+            tileList.Add(tile);
+
+            //put his adjacent tiles 
+            for (int x = tile.TileX - 1; x <= tile.TileX + 1; x++)
+            {
+                for (int y = tile.TileY - 1; y <= tile.TileY + 1; y++)
+                {
+                    if (IsInMap(x, y) && (x == tile.TileX || y == tile.TileY))
+                    {
+                        if (mapFlags[x, y] != 1 && map[x, y] == tileType)
+                        {
+                            mapFlags[x, y] = 1;
+                            queue.Enqueue(new Coord(x, y));
+                        }
+                    }
+                }
+            }
+        }
+        return tileList;
+    }
+
+    List<List<Coord>> GetRegions(int tileType)
+    {
+        List<List<Coord>> regions = new List<List<Coord>>();
+        int[,] mapFlags = new int[width, height];
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (mapFlags[x, y] == 0 && map[x, y] == tileType)
+                {
+                    regions.Add(GetRegionTiles(x, y, mapFlags));
+                }
+            }
+        }
+
+        return regions;
+    }
+
+    void ProcessMap()
+    {
+        List<List<Coord>> wallRegions = GetRegions(1);
+
+        foreach (var wallRegion in wallRegions)
+        {
+            if (wallRegion.Count < threshold)
+            {
+                foreach (var tile in wallRegion)
+                {
+                    map[tile.TileX, tile.TileY] = 0;
+                }
+            }
+        }
+
+        List<List<Coord>> floorRegions = GetRegions(0);
+        List<Room> surrivedRooms = new List<Room>();
+
+        foreach (var floorRegion in floorRegions)
+        {
+            if (floorRegion.Count < threshold)
+            {
+                foreach (var tile in floorRegion)
+                {
+                    map[tile.TileX, tile.TileY] = 1;
+                }
+            }
+            else
+            {
+                surrivedRooms.Add(new Room(floorRegion));
+            }
+        }
+
+        CalculateRoomConnections(surrivedRooms);
+    }
+
+    void CalculateRoomConnections(List<Room> surrivedRooms)
+    {
+        if (surrivedRooms.Count <= 1)
+        {
+            //only 1 room or no room
+            return;
+        }
+
+        surrivedRooms.Sort();
+
+        int[,] distances = new int[surrivedRooms.Count, surrivedRooms.Count];
+        Coord[,] closeTiles = new Coord[surrivedRooms.Count, surrivedRooms.Count];
+
+        for (int x = 0; x < surrivedRooms.Count; x++)
+        {
+            for (int y = x; y < surrivedRooms.Count; y++)
+            {
+                if (x != y)
+                {
+                    distances[x, y] = Room.CloseDistance(surrivedRooms[x], surrivedRooms[y], out closeTiles[x, y], out closeTiles[y, x]);
+                    distances[y, x] = distances[x, y];
+                }
+                else
+                {
+                    distances[x, y] = 0;
+                    closeTiles[x, y] = new Coord();
+                }
+            }
+        }
+
+        do
+        {
+            int closestRoom = 0;
+            int closestDistance = int.MaxValue;
+
+            for (int x = 0; x < surrivedRooms.Count; x++)
+            {
+                if (distances[0, x] == 0)// self or connected room
+                    continue;
+                else if (distances[0, x] < closestDistance)
+                {
+                    closestDistance = distances[0, x];
+                    closestRoom = x;
+                }
+            }
+
+            if (closestRoom == 0)
+            {
+                //no more rooms
+                break;
+            }
+            else
+            {
+                distances[0, closestRoom] = 0;
+                distances[closestRoom, 0] = 0;
+
+                for (int i = 1; i < surrivedRooms.Count; i++)
+                {
+                    if (distances[0, i] > distances[closestRoom, i])
+                    {
+                        //set distance
+                        distances[0, i] = distances[closestRoom, i];
+                        distances[i, 0] = distances[closestRoom, i];
+
+                        //set coord
+                        closeTiles[0, i] = closeTiles[closestRoom, i];
+                        closeTiles[i, 0] = closeTiles[i, closestRoom];
+                    }
+                }
+            }
+        }
+        while (true);
+
+        for (int x = 1; x < surrivedRooms.Count; x++)
+        {
+            CreatePassage(closeTiles[0, x], closeTiles[x, 0]);
+        }
+    }
+
+    void CreatePassage(Coord tileA, Coord tileB)
+    {
+        //Debug.DrawLine(CoordToWorldPoint(tileA), CoordToWorldPoint(tileB), Color.green, 5);
+
+        List<Coord> line = new List<Coord>();
+
+        int x = tileA.TileX;
+        int y = tileA.TileY;
+
+        int dx = tileB.TileX - tileA.TileX;
+        int dy = tileB.TileY - tileA.TileY;
+
+        //How many steps from A to B
+        int step;
+        float adjustX = 0.0f;
+        float adjustY = 0.0f;
+
+        if (Mathf.Abs(dx) >= Mathf.Abs(dy)) //x>=y
+        {
+            step = Mathf.Abs(dx);
+            adjustY = 0.5f;
+        }
+        else
+        {
+            step = Mathf.Abs(dy);
+            adjustX = 0.5f;
+        }
+
+        for (int i = 0; i <= step; i++)
+        {
+            line.Add(new Coord(x + (int)(dx * i / step + adjustX), y + (int)(dy * i / step + adjustY)));
+        }
+
+        foreach (var point in line)
+        {
+            DrawCircle(point, 1, map[tileA.TileX, tileA.TileY]);
+        }
+    }
+
+    private Vector3 CoordToWorldPoint(Coord tile)
+    {
+        return new Vector3(-width / 2 + .5f + tile.TileX, 2, -height / 2 + .5f + tile.TileY);
+    }
+
+    void DrawCircle(Coord point, int r, int type)
+    {
+        for (int x = -r; x <= r; x++)
+        {
+            for (int y = -r; y <= r; y++)
+            {
+                if (x * x + y * y <= r * r)// in the circle
+                {
+                    if (IsInMap(x + point.TileX, y + point.TileY))//in the map
+                    {
+                        map[x + point.TileX, y + point.TileY] = type;
+                    }
+                }
+            }
+        }
+    }
+
+    void randomFillMap()
+    {
+        if (useRandomSeed)
+        {
+            seed = Time.time.ToString();
+        }
+
+        System.Random randGenerator = new System.Random(seed.GetHashCode());
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                if (x < borderSize || y < borderSize || x >= width - borderSize || y >= height - borderSize)
+                {
+                    map[x, y] = 1;
+                }
+                else
+                {
+                    map[x, y] = randGenerator.Next(0, 100) < randomFillPercent ? 1 : 0;
+                }
+            }
+        }
+    }
+
+    void SmoothMap()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                int wallCount = GetSurroundingCount(x, y);
+                if (wallCount > 4)
+                {
+                    map[x, y] = 1;
+                }
+                else if (wallCount < 4)
+                {
+                    map[x, y] = 0;
+                }
+            }
+        }
+    }
+
+    int GetSurroundingCount(int gridX, int gridY)
+    {
+        int wallCount = 0;
+
+        for (int neighbourX = gridX - 1; neighbourX <= gridX + 1; neighbourX++)
+        {
+            for (int neighbourY = gridY - 1; neighbourY <= gridY + 1; neighbourY++)
+            {
+                if (IsInMap(neighbourX, neighbourY))
+                {
+                    if (neighbourX != gridX || neighbourY != gridY)
+                    {
+                        wallCount += map[neighbourX, neighbourY];
+                    }
+                }
+                else
+                {
+                    wallCount++;
+                }
+            }
+        }
+        return wallCount;
+    }
+
+    private bool IsInMap(int x, int y)
+    {
+        return x >= 0 && x < width && y >= 0 && y < height;
+    }
+
+    struct Coord
+    {
+        public int TileX;
+        public int TileY;
+
+        public Coord(int x, int y)
+        {
+            TileX = x;
+            TileY = y;
+        }
+    }
+
+    class Room : System.IComparable<Room>
+    {
+        public List<Coord> tiles;
+        public List<Coord> edges;
+
+        public int roomSize;
+
+        public Room()
+        {
+
+        }
+
+        public Room(List<Coord> roomTiles)
+        {
+            tiles = roomTiles;
+            roomSize = roomTiles.Count;
+
+            HashSet<Coord> tilesHashSet = new HashSet<Coord>();
+            foreach (var tile in roomTiles)
+            {
+                tilesHashSet.Add(tile);
+            }
+
+            edges = new List<Coord>();
+
+            foreach (var tile in roomTiles)
+            {
+                for (int x = tile.TileX - 1; x <= tile.TileX + 1; x++)
+                {
+                    for (int y = tile.TileY - 1; y <= tile.TileY + 1; y++)
+                    {
+                        if (x == tile.TileX || y == tile.TileY)
+                        {
+                            if (!tilesHashSet.Contains(new Coord(x, y)))
+                                edges.Add(tile);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static int CloseDistance(Room roomA, Room roomB, out Coord tileA, out Coord tileB)
+        {
+            int bestDistance = int.MaxValue;
+
+            tileA = new Coord();
+            tileB = new Coord();
+
+            foreach (var roomATile in roomA.edges)
+            {
+                foreach (var roomBTile in roomB.edges)
+                {
+                    var distance = (int)(Mathf.Pow((roomATile.TileX - roomBTile.TileX), 2) + Mathf.Pow((roomATile.TileY - roomBTile.TileY), 2));
+                    if (distance < bestDistance)
+                    {
+                        bestDistance = distance;
+                        tileA = roomATile;
+                        tileB = roomBTile;
+                    }
+                }
+            }
+            return bestDistance;
+        }
+
+        public int CompareTo(Room other)
+        {
+            return other.roomSize.CompareTo(this.roomSize);
+        }
+    }
+}
---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,410 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MeshGenerator : MonoBehaviour
+{
+    public SquareGrid squareGrid;
+
+    public MeshFilter floors;
+    public MeshFilter walls;
+
+    public float wallHeight = 5.0f;
+
+    List<Vector3> vertics;
+    List<int> triangles;
+
+    Dictionary<int, List<Triangle>> trianglesDictionary;
+    List<List<int>> outLines;
+
+    HashSet<int> checkedVertices;
+
+    public void GenerateMesh(int[,] map, float squareSize)
+    {
+        vertics = new List<Vector3>();
+        triangles = new List<int>();
+
+        trianglesDictionary = new Dictionary<int, List<Triangle>>();
+        outLines = new List<List<int>>();
+        checkedVertices = new HashSet<int>();
+
+        squareGrid = new SquareGrid(map, squareSize);
+
+        CreateFloorMesh();
+        CreateWallMesh();
+    }
+
+    void CreateFloorMesh()
+    {
+        for (int x = 0; x < squareGrid.squares.GetLength(0); x++)
+        {
+            for (int y = 0; y < squareGrid.squares.GetLength(1); y++)
+            {
+                TriangulateSquare(squareGrid.squares[x, y]);
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        floors.mesh = mesh;
+
+        mesh.vertices = vertics.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+    void CreateWallMesh()
+    {
+        GetOutLines();
+
+        List<Vector3> wallVertics = new List<Vector3>();
+        List<int> wallTriangles = new List<int>();
+
+        foreach (var outLine in outLines)
+        {
+            for (int i = 0; i < outLine.Count - 1; i++)
+            {
+                int startIndex = wallVertics.Count;
+                wallVertics.Add(vertics[outLine[i]]);
+                wallVertics.Add(vertics[outLine[i + 1]]);
+                wallVertics.Add(vertics[outLine[i]] - Vector3.up * wallHeight);
+                wallVertics.Add(vertics[outLine[i + 1]] - Vector3.up * wallHeight);
+
+                wallTriangles.Add(startIndex + 0);
+                wallTriangles.Add(startIndex + 2);
+                wallTriangles.Add(startIndex + 3);
+
+                wallTriangles.Add(startIndex + 0);
+                wallTriangles.Add(startIndex + 3);
+                wallTriangles.Add(startIndex + 1);
+            }
+        }
+
+        Mesh wallMesh = new Mesh();
+        walls.mesh = wallMesh;
+
+        wallMesh.vertices = wallVertics.ToArray();
+        wallMesh.triangles = wallTriangles.ToArray();
+        wallMesh.RecalculateNormals();
+    }
+
+    void TriangulateSquare(Square square)
+    {
+        switch (square.configuration)
+        {
+            case 0:
+                break;
+
+            // 1 points:
+            case 1:
+                square.centerLeft.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                MeshFromPoints(square.centerLeft, square.centerBottom, square.bottomLeft);
+                break;
+            case 2:
+                square.centerBottom.isOutterLine = true;
+                square.centerRight.isOutterLine = true;
+                MeshFromPoints(square.bottomRight, square.centerBottom, square.centerRight);
+                break;
+            case 4:
+                square.centerRight.isOutterLine = true;
+                square.centerTop.isOutterLine = true;
+                MeshFromPoints(square.topRight, square.centerRight, square.centerTop);
+                break;
+            case 8:
+                square.centerTop.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.centerTop, square.centerLeft);
+                break;
+
+
+            // 2 points:
+            case 3:
+                square.centerLeft.isOutterLine = true;
+                square.centerRight.isOutterLine = true;
+                MeshFromPoints(square.bottomRight, square.bottomLeft, square.centerLeft, square.centerRight);
+                break;
+            case 6:
+                square.centerTop.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                MeshFromPoints(square.centerTop, square.topRight, square.bottomRight, square.centerBottom);
+                break;
+            case 9:
+                square.centerTop.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.centerTop, square.centerBottom, square.bottomLeft);
+                break;
+            case 12:
+                square.centerRight.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.topRight, square.centerRight, square.centerLeft);
+                break;
+            case 5:
+                square.centerTop.isOutterLine = true;
+                square.centerRight.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.centerTop, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft, square.centerLeft);
+                break;
+            case 10:
+                square.centerTop.isOutterLine = true;
+                square.centerRight.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.centerBottom, square.centerLeft);
+                break;
+
+            // 3 point:
+            case 7:
+                square.centerTop.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.centerTop, square.topRight, square.bottomRight, square.bottomLeft, square.centerLeft);
+                break;
+            case 11:
+                square.centerTop.isOutterLine = true;
+                square.centerRight.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.centerTop, square.centerRight, square.bottomRight, square.bottomLeft);
+                break;
+            case 13:
+                square.centerRight.isOutterLine = true;
+                square.centerBottom.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.topRight, square.centerRight, square.centerBottom, square.bottomLeft);
+                break;
+            case 14:
+                square.centerBottom.isOutterLine = true;
+                square.centerLeft.isOutterLine = true;
+                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.centerBottom, square.centerLeft);
+                break;
+
+            // 4 point:
+            case 15:
+                MeshFromPoints(square.topLeft, square.topRight, square.bottomRight, square.bottomLeft);
+                break;
+        }
+    }
+
+    void MeshFromPoints(params Node[] points)
+    {
+        AssignVertics(points);
+
+        if (points.Length >= 3)
+            CreateTriangles(points[0], points[1], points[2]);
+        if (points.Length >= 4)
+            CreateTriangles(points[0], points[2], points[3]);
+        if (points.Length >= 5)
+            CreateTriangles(points[0], points[3], points[4]);
+        if (points.Length >= 6)
+            CreateTriangles(points[0], points[4], points[5]);
+    }
+
+    void AssignVertics(Node[] points)
+    {
+        for (int i = 0; i < points.Length; i++)
+        {
+            if (points[i].vertexIndex == -1)
+            {
+                points[i].vertexIndex = vertics.Count;
+                vertics.Add(points[i].position);
+            }
+        }
+    }
+
+    void CreateTriangles(Node a, Node b, Node c)
+    {
+        triangles.Add(a.vertexIndex);
+        triangles.Add(b.vertexIndex);
+        triangles.Add(c.vertexIndex);
+
+        Triangle triangle = new Triangle(a.vertexIndex, b.vertexIndex, c.vertexIndex);
+
+        if (a.isOutterLine)
+        {
+            AddTriangleToDictionary(a.vertexIndex, triangle);
+        }
+        if (b.isOutterLine)
+        {
+            AddTriangleToDictionary(b.vertexIndex, triangle);
+        }
+        if (c.isOutterLine)
+        {
+            AddTriangleToDictionary(c.vertexIndex, triangle);
+        }
+    }
+
+    void AddTriangleToDictionary(int vertexIndexKey, Triangle triangle)
+    {
+        if (trianglesDictionary.ContainsKey(vertexIndexKey))
+        {
+            //Contains the vertexIndexKey, Add the new triangle to the list of the index
+            trianglesDictionary[vertexIndexKey].Add(triangle);
+        }
+        else
+        {
+            //Doesn't contain the vertexIndexKey, create a new key and add the triangle
+            trianglesDictionary.Add(vertexIndexKey, new List<Triangle>());
+            trianglesDictionary[vertexIndexKey].Add(triangle);
+        }
+    }
+
+    int NextOutLineVertex(int vertexIndex)
+    {
+        var triangleList = trianglesDictionary[vertexIndex];
+
+        int nextVertexIndex = -1;
+
+        foreach (var triangle in triangleList)
+        {
+            nextVertexIndex = triangle.NextVertexClockwise(vertexIndex);
+
+            //GetNextVertexIndexClockwise
+            if (trianglesDictionary.ContainsKey(nextVertexIndex))
+            {
+                break;
+            }
+        }
+        return nextVertexIndex;
+    }
+
+    List<int> GetOutLine(int vertexStart)
+    {
+        List<int> verticesList = new List<int>();
+        verticesList.Add(vertexStart);
+
+        int nextVertexIndex = vertexStart;
+        while (true)
+        {
+            checkedVertices.Add(nextVertexIndex);
+            nextVertexIndex = NextOutLineVertex(nextVertexIndex);
+            verticesList.Add(nextVertexIndex);
+            if (nextVertexIndex == vertexStart)
+            {
+                break;
+            }
+        }
+        return verticesList;
+    }
+
+    void GetOutLines()
+    {
+        foreach (var vertex in trianglesDictionary)
+        {
+            if (!checkedVertices.Contains(vertex.Key))
+            {
+                outLines.Add(GetOutLine(vertex.Key));
+            }
+        }
+    }
+
+    public class SquareGrid
+    {
+        public Square[,] squares;
+
+        public SquareGrid(int[,] map, float squareSize)
+        {
+            int nodeCountX = map.GetLength(0);
+            int nodeCountY = map.GetLength(1);
+
+            float mapWidth = nodeCountX * squareSize;
+            float mapHeight = nodeCountY * squareSize;
+
+            ControlNode[,] controlNodes = new ControlNode[nodeCountX, nodeCountY];
+            for (int x = 0; x < nodeCountX; x++)
+            {
+                for (int y = 0; y < nodeCountY; y++)
+                {
+                    controlNodes[x, y] = new ControlNode(new Vector3(-mapWidth / 2 + x * squareSize + squareSize / 2f, 0, -mapHeight / 2 + y * squareSize + squareSize / 2f), map[x, y] == 1, squareSize);
+                }
+            }
+
+            squares = new Square[nodeCountX - 1, nodeCountY - 1];
+            for (int x = 0; x < nodeCountX - 1; x++)
+            {
+                for (int y = 0; y < nodeCountY - 1; y++)
+                {
+                    squares[x, y] = new Square(controlNodes[x, y + 1], controlNodes[x + 1, y + 1], controlNodes[x, y], controlNodes[x + 1, y]);
+                }
+            }
+        }
+    }
+
+    public class Square
+    {
+        public ControlNode topLeft, topRight, bottomLeft, bottomRight;
+        public Node centerTop, centerLeft, centerRight, centerBottom;
+
+        public int configuration = 0;
+
+        public Square(ControlNode _topLeft, ControlNode _topRight, ControlNode _bottomLeft, ControlNode _bottomRight)
+        {
+            topLeft = _topLeft;
+            topRight = _topRight;
+            bottomLeft = _bottomLeft;
+            bottomRight = _bottomRight;
+
+            centerTop = topLeft.right;
+            centerLeft = bottomLeft.above;
+            centerRight = bottomRight.above;
+            centerBottom = bottomLeft.right;
+
+            if (topLeft.active)
+                configuration += 8;
+            if (topRight.active)
+                configuration += 4;
+            if (bottomRight.active)
+                configuration += 2;
+            if (bottomLeft.active)
+                configuration += 1;
+        }
+    }
+
+    public class Node
+    {
+        public Vector3 position;
+        public int vertexIndex = -1;
+        public bool isOutterLine = false;
+
+        public Node(Vector3 _pos)
+        {
+            position = _pos;
+        }
+    }
+
+    public class ControlNode : Node
+    {
+        public bool active;
+
+        public Node above, right;
+
+        public ControlNode(Vector3 _pos, bool _active, float squareSize)
+            : base(_pos)
+        {
+            active = _active;
+            above = new Node(_pos + Vector3.forward * squareSize / 2f);
+            right = new Node(_pos + Vector3.right * squareSize / 2f);
+        }
+    }
+
+    struct Triangle
+    {
+        public int VertexIndexA;
+        public int VertexIndexB;
+        public int VertexIndexC;
+
+        public Triangle(int a, int b, int c)
+        {
+            VertexIndexA = a;
+            VertexIndexB = b;
+            VertexIndexC = c;
+        }
+
+        public int NextVertexClockwise(int vertexIndex)
+        {
+            if (vertexIndex == VertexIndexA)
+                return VertexIndexB;
+            else if (vertexIndex == VertexIndexB)
+                return VertexIndexC;
+            else if (vertexIndex == VertexIndexC)
+                return VertexIndexA;
+            else
+                return -1;
+        }
+    }
+}
